d4639c151e297cf75af888e0ac1d7d0be88af837
Nikolay <103648794+gilotin@users.noreply.github.com>
2024-11-27 23:42:54 +0200

    fix: minor issues with HTML and starting to build more flexible checkError function.

--- app.js
+++ app.js
@@ -1,72 +1,56 @@
-import getAge from './getAge.js';
-
+import getAge from "./getAge.js";
 
 const form = document.querySelector(".form");
 const submitButton = document.querySelector(".form__button");
 
-
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const resultYears = document.querySelector(".result__years--purple");
-  const resultMonths = document.querySelector(".result__months--purple");
-  const resultDays = document.querySelector(".result__days--purple");
+    e.preventDefault();
+    const resultYears = document.querySelector(".result__years--purple");
+    const resultMonths = document.querySelector(".result__months--purple");
+    const resultDays = document.querySelector(".result__days--purple");
 
-const testForm = document.forms.testForm
+    const testForm = document.forms.testForm;
 
+    const formData = new FormData(e.target);
 
+    const inputData = Object.fromEntries(formData);
 
-  const formData = new FormData(e.target)
-  
-  const inputData = Object.fromEntries(formData)
-  
+    errorCheck(inputData);
 
-   errorCheck(inputData)
+    const result = getAge(inputData);
 
+    resultYears.textContent = `${result.years}`;
+    resultMonths.textContent = `${result.months}`;
+    resultDays.textContent = `${result.days}`;
+});
 
-  const result = getAge(inputData);
-  
+const errorLabelRequired = document.querySelector(".form__error-required");
+const errorLabelValidate = document.querySelector(".form__error-validation");
+const formLabel = document.querySelector(".form__label");
 
-  resultYears.textContent = `${result.years}`
-  resultMonths.textContent = `${result.months}`
-  resultDays.textContent = `${result.days}`
-
-
-
-})
-
-
-const errorLableRequired = document.querySelector(".form__error-required");
-const errorLableValidate = document.querySelector(".form__error-validation");
-
-// fix the function 
+// fix the function
 
 function errorCheck(inputData) {
-  
-  let day = Number(inputData.day);
-  let month = Number(inputData.month)
-  let year = Number(inputData.year)
-  
-  
-  
-  if(day == ""){
-  errorLableRequired.style.display = "block"
-  errorLableValidate.style.display = "none"
-    return
-    
-  }else{
-   errorLableRequired.style.display = "none";
-   
-  }
-  
- 
-  
-  if( checkedDate <= 0 || checkedDate > 31){
-    errorLableValidate.style.display = "block"
-     errorLableRequired.style.display = "none"
-     return
-     
-  }else{
-  errorLableValidate.style.display = "none"
-  
-  }
+    let day = Number(inputData.day);
+    let month = Number(inputData.month);
+    let year = Number(inputData.year);
+
+    for (let element in inputData) {
+        // NOTE: NEED more work . Tomorrow !
+
+        if (inputData[element] == "") {
+            errorLabelRequired.style.display = "block";
+            errorLabelValidate.style.display = "none";
+            formLabel.style.color = "red";
+        } else {
+            errorLabelRequired.style.display = "none";
+        }
+
+        // if (day <= 0 || day > 31) {
+        //     errorLabelValidate.style.display = "block";
+        //     errorLabelRequired.style.display = "none";
+        // } else {
+        //     errorLabelValidate.style.display = "none";
+        // }
+    }
 }
