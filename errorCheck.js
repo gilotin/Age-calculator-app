daeb2ea7f432ce6f30b8be049373b7afd7aa53ca
Nikolay <103648794+gilotin@users.noreply.github.com>
2024-12-20 14:28:52 +0200

    feat: adding more error label visualization while submitting empty sting

--- errorCheck.js
+++ errorCheck.js
@@ -1,9 +1,10 @@
 export function errorCheck(inputData) {
     let hasError = false;
-
     for (let element in inputData) {
         const errorValidationLabel = document.querySelector(`.form__error-validation-${element}`);
         const errorLabelRequired = document.querySelector(`.form__error-required-${element}`);
+        const formLabel = document.querySelectorAll(".form__label");
+        const formInput = document.querySelector(".form__input");
 
         const date = Number(inputData[element]);
         const currentDate = new Date();
@@ -11,33 +12,44 @@ export function errorCheck(inputData) {
         const month = inputData["month"];
         const year = inputData["year"];
 
-        if (inputData[element] == "") {
+        if (inputData[element] === "") {
             errorLabelRequired.style.display = "block";
             errorValidationLabel.style.display = "none";
+            formLabel.forEach((e) => (e.style.color = "red"));
             hasError = true;
         } else {
             errorLabelRequired.style.display = "none";
-            hasError = false;
+            formLabel.forEach((e) => (e.style.color = "lightgrey"));
 
             if (element === "year" && (date < 1900 || date > presentYear)) {
                 errorValidationLabel.style.display = "block";
-                return (hasError = true);
-            } else if (element == "month" && (date < 1 || date > 12)) {
+
+                hasError = true;
+            } else if (element === "year") {
+                errorValidationLabel.style.display = "none";
+            }
+
+            if (element === "month" && (date < 1 || date > 12)) {
                 errorValidationLabel.style.display = "block";
-                return (hasError = true);
-            } else if (element == "day" && (date <= 0 || date > daysMaxRange(month, year))) {
+                hasError = true;
+            } else if (element === "month") {
+                errorValidationLabel.style.display = "none";
+            }
+
+            if (element === "day" && (date <= 0 || date > daysMaxRange(month, year))) {
                 errorValidationLabel.style.display = "block";
-                return (hasError = true);
-            } else {
+                hasError = true;
+            } else if (element === "day") {
                 errorValidationLabel.style.display = "none";
             }
         }
     }
+
     return hasError;
 }
 
 function daysMaxRange(month, year) {
-    let thirtyDayMonths = ["4", "6", "9", "11"];
+    const thirtyDayMonths = ["4", "6", "9", "11"];
 
     if (isLeapYear(year) && month == 2) {
         return 29;
